e9e65acb0201971acf1e5153627ddf79d0c7aee0
ChaonengTan <chaonengtan@gmail.com>
2020-11-30 18:43:25 -0800

    +disc

--- src/components/Index.js
+++ src/components/Index.js
@@ -1,9 +1,16 @@
-import { NavLink } from 'react-router-dom'
-
 function Index() {
   return (
     <div>
-        <h1>wElCoMe To ThE hOmE pAgE</h1>
+      <div>
+        <h1>wElCoMe To My ReAcT gAmE sItE!</h1><br/>
+        <p>How to get started:</p>
+        <p>Just click on one of the game tabs in the Navbar!</p>
+      </div><br/><br/>
+      <div>
+        <h2>aBoUt ThIs PaGe!</h2><br/>
+        <p>This site was created for the sole purpose of YES!</p>
+        <p>ps. just saying, my gity huby is on the top right of the nav... just saying...</p>
+      </div>
     </div>
   )
 }
